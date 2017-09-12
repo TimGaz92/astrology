ce40c2fce9af4a04de104d565bc133984a6d11cb
Vivek Jain <Vivek Jain>
2017-09-12 07:52:41 -0400

    cleaning up the code

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,6 @@ var express = require('express');
 var passport = require('passport');
 var fs = require("fs");
 
-
 // setup router
 var router = express.Router();
 
@@ -16,28 +15,23 @@ router.route('/sign-up')
 		res.render('sign-up', { title: 'Astrology - Sign Up' });
 	})
 	.post(passport.authenticate('local-signup'), function(req, res){
-		console.log("in route signup req.user -");
-		// console.log(req);
 		// pass the current user object to the server
 		server.getCurrentUser(req.user);
 		res.redirect ('/horoscope');
-		// failureRedirect: '/sign-up'
 	});
 
 router.route('/login')
 	.get(function(req, res) {
 		res.render('login', { title: 'Astrology - Login' });
 	})
+
 	.post(passport.authenticate('local-login'), function(req, res){
-		console.log(req.user);
 		// pass the current user object to the server
 		server.getCurrentUser(req.user);
 		res.redirect( '/horoscope')
 	});
 
 function showCurrentStuff(){
-		console.log("we're in the login route");
-		console.log(res);
 		failureRedirect: '/login'
 };
 
@@ -48,17 +42,14 @@ router.get('/logout', function(req, res) {
 	});
 });
 
-router.get('/user', isLoggedIn, function(req, res) {
 
+router.get('/user', isLoggedIn, function(req, res) {
 	res.render('user', { title: 'MovieGoers - '+req.user.username, username: req.user.username })
 
 });
 
 
 module.exports = router;
-
-// module.exports = function Routes(currentUser){};
-
 // function to test if user is logged in
 function isLoggedIn(req, res, next) {
 	if (req.isAuthenticated()) {
